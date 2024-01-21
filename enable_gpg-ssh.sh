d259527244a3928ad73ae97f3b65102860c6ef8b
deftclaw <deftclaw@briskbear.rocks>
2024-01-20 21:40:28 -0700

    Create install script, move all sudo commands to install, formatting fixes

--- enable_gpg-ssh.sh
+++ enable_gpg-ssh.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 echo 'enable-ssh-support' >> $HOME/.gnupg/gpg-agent.conf
-sudo cp gpg_ssh.sh /etc/profile.d/
 
 keygrip=`gpg -k --with-keygrip|tail -n 2|head -n 1|awk '{print $NF}'`
 echo "${keygrip} 5" >> $HOME/.gnupg/sshcontrol
--- gpg_backup.sh
+++ gpg_backup.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 [[ $# -eq 1 ]] || return 1
 key="$1"
-gpg -a --export-secret-keys $key > ${key}.pem.asc
 
+gpg -a --export-secret-keys "$key" > "${key}.pem.asc"
 gpg --export-ownertrust > ownertrust.txt
