43c120cb94aa8c28e114379a2e0c284e8996a6e6
deftclaw <deftclaw@briskbear.rocks>
2024-02-02 20:22:22 -0700

    Add comments, add gpg as git signing key

--- enable_gpg-ssh.sh
+++ enable_gpg-ssh.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+# Enable using gpg-key for ssh-authentication
 echo 'enable-ssh-support' >> $HOME/.gnupg/gpg-agent.conf
 
-keygrip=`gpg -k --with-keygrip|tail -n 2|head -n 1|awk '{print $NF}'`
+# Add pgp key to the list of ssh keys
+keygrip=`gpg -k --with-keygrip|tail -n 3|head -n 1|awk '{print $NF}'`
 echo "${keygrip} 5" >> $HOME/.gnupg/sshcontrol
 ssh-add -l
+
+# Enable pgp signing of git commits
+git config --global --unset gpg.format
+keyID=`gpg -K --keyid-format=long|tail -n 4|head -n 1|awk -F' +|/' '{print $3}'`
+git config --global user.signingkey ${keyID}
 
 echo -e "\033[38;5;42mRemember to \`gpg --export-ssh-key\`\033[0m"
